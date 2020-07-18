c2f93b8ee2c00d85cd096f7f5bfb50c5e3af0c39
Shreyas S Kumar <shreyassreekripa@gmail.com>
2020-07-18 22:50:11 +0530

    Update questionClass.dart

--- lib/questionClass.dart
+++ lib/questionClass.dart
@@ -2,9 +2,6 @@ class Question {
   String qstn, img;
   int ans;
   List<String> option = List();
-<<<<<<< HEAD
   Question(this.qstn, this.ans, this.option, {this.img});
-=======
-  Question(this.qstn, this.ans, this.option, this.img);
->>>>>>> 83d03e9b450bb6152585f1aca7ff34bf4e382401
+
 }
