83258bfaeae62da03f7c2e747d4bf3243cbd195a
Shreyas S Kumar <shreyassreekripa@gmail.com>
2020-07-20 17:52:25 +0530

    Mock update

--- lib/mock.dart
+++ lib/mock.dart
@@ -10,6 +10,7 @@ class Mock extends StatefulWidget {
     return _MockState();
   }
 }
+
 class _MockState extends State<Mock> {
   int selectedOption;
 
@@ -37,26 +38,22 @@ class _MockState extends State<Mock> {
 
   int _totalScore = 0;
   int ans;
-  final Duration _duration = const Duration(minutes: 20);
+
   var _index = 0;
   var random = new Random();
   var _questionIndex = [];
 
   void _answerQuestion() {
-
-
-    if(selectedOption-1 == english[_questionIndex[_index]].ans)
+    if (selectedOption - 1 == english[_questionIndex[_index]].ans)
       _totalScore++;
     print("Score:$_totalScore");
-    if(selectedOption == 0 )
+    if (selectedOption == 0)
       _showToast(context);
     else
       setState(() {
-      selectedOption = 0;
-      _index++;
-    });
-
-
+        selectedOption = 0;
+        _index++;
+      });
   }
 
   void _generateQuestion() {
@@ -74,89 +71,58 @@ class _MockState extends State<Mock> {
   Widget build(BuildContext context) {
     // TODO: implement build
     _generateQuestion();
-    return _index < 20 ? Container(
-      child: english[_questionIndex[_index]].img == null ? Column(
-        children: <Widget>[
-          ListTile(
-            title: Text((_index + 1).toString()+"." + english[_questionIndex[_index]].qstn,
-                style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold)
+    return _index < 20
+        ? Container(
+            child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                    (_index + 1).toString() +
+                        "." +
+                        english[_questionIndex[_index]].qstn,
+                    style:
+                        TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+              ),
+              if (english[_questionIndex[_index]].img != null)
+                Image.asset(
+                    'assets/images/' + english[_questionIndex[_index]].img),
+              RadioListTile(
+                title: Text(english[_questionIndex[_index]].option[0]),
+                value: 1,
+                groupValue: selectedOption,
+                onChanged: (val) {
+                  setSelectedOption(val);
+                },
+              ),
+              RadioListTile(
+                title: Text(english[_questionIndex[_index]].option[1]),
+                value: 2,
+                groupValue: selectedOption,
+                onChanged: (val) {
+                  setSelectedOption(val);
+                },
+              ),
+              RadioListTile(
+                title: Text(english[_questionIndex[_index]].option[2]),
+                value: 3,
+                groupValue: selectedOption,
+                onChanged: (val) {
+                  setSelectedOption(val);
+                },
+              ),
+              RaisedButton(
+                child: Text("Next"),
+                color: Colors.green,
+                onPressed: _answerQuestion,
+              )
+            ],
+          ))
+        : Container(
+            child: Center(
+            child: Text(
+              "Test Completed\nYour Score : $_totalScore",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
-          ),
-          RadioListTile(
-            title: Text(english[_questionIndex[_index]].option[0]),
-            value: 1,
-            groupValue: selectedOption,
-            onChanged: (val) {
-              setSelectedOption(val);
-            },
-          ),
-          RadioListTile(
-            title: Text(english[_questionIndex[_index]].option[1]),
-            value: 2,
-            groupValue: selectedOption,
-            onChanged: (val) {
-              setSelectedOption(val);
-            },),
-          RadioListTile(
-            title: Text(english[_questionIndex[_index]].option[2]),
-            value: 3,
-            groupValue: selectedOption,
-            onChanged: (val) {
-              setSelectedOption(val);
-            },),
-          RaisedButton(
-            child: Text("Next"),
-            color: Colors.green,
-            onPressed: _answerQuestion,
-          )
-
-        ],
-      ):
-      Column(
-        children: <Widget>[
-          ListTile(
-            title: Text((_index + 1).toString()+"." + english[_questionIndex[_index]].qstn,
-                style: TextStyle(fontSize: 17,fontWeight: FontWeight.bold)
-            ),
-          ),
-          Image.asset('assets/images/'+english[_questionIndex[_index]].img),
-          RadioListTile(
-            title: Text(english[_questionIndex[_index]].option[0]),
-            value: 1,
-            groupValue: selectedOption,
-            onChanged: (val) {
-              setSelectedOption(val);
-            },
-          ),
-          RadioListTile(
-            title: Text(english[_questionIndex[_index]].option[1]),
-            value: 2,
-            groupValue: selectedOption,
-            onChanged: (val) {
-              setSelectedOption(val);
-            },),
-          RadioListTile(
-            title: Text(english[_questionIndex[_index]].option[2]),
-            value: 3,
-            groupValue: selectedOption,
-            onChanged: (val) {
-              setSelectedOption(val);
-            },),
-          RaisedButton(
-            child: Text("Next"),
-            color: Colors.green,
-            onPressed: _answerQuestion,
-          )
-
-        ],
-      ),
-    ):
-        Container(
-          child: Center(
-            child: Text("Test Completed\nYour Score : $_totalScore",
-            style: TextStyle(fontWeight: FontWeight.bold,
-            fontSize: 20),),
-          )
-        );
+          ));
   }
 }
